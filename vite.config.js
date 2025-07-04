8822c02e1213e577b53673aa510afaf22ed38cb8
Helen Virtanen <helenvirtanen28@gmail.com>
2025-07-04 22:50:50 +0300

    fix: add cross-env

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+const base = import.meta.env.DEPLOY_TARGET === 'gh' ? '/foodrestaurant/' : '/'
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/food-asia-react/",
+  base,
   plugins: [
     react(), 
     tailwindcss()
